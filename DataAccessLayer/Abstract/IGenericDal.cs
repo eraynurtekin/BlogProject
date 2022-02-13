0825c5e1a91f2a953406ed0ad1509b98ef741b9f
Eray Nurtekin <eray.nurtekin@hotmail.com>
2022-02-13 12:33:31 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        // T bir class a ait tüm özellikleri kullanacak.
+
+        void Insert(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetListAll();
+        T GetByID(int id);
+
+    }
+}
